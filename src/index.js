ce9b8960ff542746d9d2e7a07cc4e1007beec0b3
Anton <toshi4ka7@mail.ru>
2020-10-01 22:25:04 +0300

    feat: ready task

--- src/index.js
+++ src/index.js
@@ -1,12 +1,21 @@
 
 exports.min = function min (array) {
-  return 0;
+  if (arguments.length === 0 || array.length === 0) {
+    return 0;
+  }
+  return array.sort((a,b) => b - a).pop();
 }
 
 exports.max = function max (array) {
-  return 0;
+  if (arguments.length === 0 || array.length === 0) {
+    return 0;
+  }
+  return array.sort((a,b) => a - b).pop();
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  if (arguments.length === 0 || array.length === 0) {
+    return 0;
+  }
+  return array.reduce((accumulator, currentValue) => accumulator + currentValue) / array.length;
 }
